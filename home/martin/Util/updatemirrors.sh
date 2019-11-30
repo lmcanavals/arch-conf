1d33fc3a275de426f433e6c8b878363d1a8eeee7
Luis Martín Canaval Sánchez <uno@canaval.org>
2019-11-30 21:12:06 +0000

    little thing on updatemirrors

--- home/martin/Util/updatemirrors.sh
+++ home/martin/Util/updatemirrors.sh
@@ -1,11 +1,21 @@
-if [ -f /etc/pacman.d/mirrorlist.pacnew ]; then
-  sed '/#Server/ s|#|| ' -i /etc/pacman.d/mirrorlist.pacnew
-  sed '/^#.*$/d' -i /etc/pacman.d/mirrorlist.pacnew
-  rankmirrors -n 6 /etc/pacman.d/mirrorlist.pacnew > /etc/pacman.d/mirrorlist
-  if [ $? -eq 0 ]; then
-    rm /etc/pacman.d/mirrorlist.pacnew
-    echo "Mirror list updated"
-  else
-    echo "Failed to update mirror list"
-  fi
-fi
+safeupdatemirrors() {
+    local base
+    local tempfile
+    base="/etc/pacman.d"
+    tempfile="/tmp/supertemomirror987654"
+    if [ -f $base/etc/pacman.d/mirrorlist.pacnew ]; then
+        sed '/#Server/ s|#|| ' -i $base/mirrorlist.pacnew
+        sed '/^#.*$/d' -i $base/mirrorlist.pacnew
+        cp $base/mirrorlist $tempfile
+        rankmirrors -n 6 $base/mirrorlist.pacnew > $base/mirrorlist
+        if [ $? -eq 0 ]; then
+           rm $base/mirrorlist.pacnew
+            echo "Mirror list updated"
+        else
+            cp $tempfile $base/mirrorlist
+            echo "Failed to update mirror list"
+        fi
+        rm $tempfile
+    fi
+}
+safeupdatemirrors
