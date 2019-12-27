f5d375b2cfebb42f9c55ce79fe0935afd941978c
Luis Martín Canaval Sánchez <uno@canaval.org>
2019-12-27 09:38:00 +0100

    .zsh gone

--- home/martin/.zsh/paths.sh
+++
@@ -1,9 +0,0 @@
-#export JAVA_HOME="/home/martin/Downloads/jdk-10.0.2"
-#export MVN_HOME="/home/martin/Downloads/maven"
-#export CLASSPATH=".:$JAVA_HOME/lib"
-#export PATH="$JAVA_HOME/bin:$MVN_HOME/bin:$PATH"
-GOGO=~/go
-export PATH="$GOGO/bin:$PATH"
-
-unset GOGO
-
